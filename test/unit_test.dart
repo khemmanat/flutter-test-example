19eb5306af7f9effd1023d3690d7e033d1c9b584
Khemmanat Kim <khemmanat2012@gmail.com>
2022-06-15 22:39:31 +0700

    group testing cases

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -3,15 +3,19 @@ import 'package:flutter_test_example/features/functions/counter.dart';
 // import 'package:flutter_test_example/lib/counter.dart'
 
 void main() {
-  test('Testing the increment counter', () {
-    Counter counter = Counter();
-    int res = counter.increment(3);
-    expect(res, 4);
-  });
+  group("Testing Counters", () {
+    test('Testing the increment counter', () {
+      Counter counter = Counter();
+      int res = counter.increment(3);
+      expect(res, 4);
+      expect(res, isNot(2));
+    });
 
-  test('Testing the decrement counter', () {
-    Counter counter = Counter();
-    int res = counter.decrement(6);
-    expect(res, 5);
+    test('Testing the decrement counter', () {
+      Counter counter = Counter();
+      int res = counter.decrement(6);
+      expect(res, 5);
+      expect(res, isNot(7));
+    });
   });
 }
