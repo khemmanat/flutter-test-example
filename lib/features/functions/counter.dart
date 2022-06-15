055f640ba5c1e66a44c08e0716e9d8e9816813ae
Khemmanat Kim <khemmanat2012@gmail.com>
2022-06-15 22:33:50 +0700

    add simple unit test

---
+++ lib/features/functions/counter.dart
@@ -0,0 +1,4 @@
+class Counter {
+  int increment(int x) => x + 1;
+  int decrement(int x) => x - 1;
+}
--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,1 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test_example/features/functions/counter.dart';
+// import 'package:flutter_test_example/lib/counter.dart'
+
+void main() {
+  test('Testing the increment counter', () {
+    Counter counter = Counter();
+    int res = counter.increment(3);
+    expect(res, 4);
+  });
+
+  test('Testing the decrement counter', () {
+    Counter counter = Counter();
+    int res = counter.decrement(6);
+    expect(res, 5);
+  });
+}
